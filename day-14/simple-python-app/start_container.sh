11cf7e517f5087ebdd7e7913eab7aa5ecaa55ec9
Niharikareddy-d <danduniharikareddy@gmail.com>
2025-10-22 14:47:22 +0530

    Fix syntax errors in start_container.sh

--- day-14/simple-python-app/start_container.sh
+++ day-14/simple-python-app/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-echo
+docker pull danduniharika30/simple-python-flask-app
 
 # Run the Docker image as a container
-echo
+docker run -d -p 5000:5000 danduniharika30/simple-python-flask-app
