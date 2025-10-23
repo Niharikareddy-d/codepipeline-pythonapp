5fcfb26532f157e56ba33485fcb78665957d8b0e
Niharikareddy-d <danduniharikareddy@gmail.com>
2025-10-23 12:54:28 +0530

    Update start_container.sh

--- day-14/simple-python-app/start_container.sh
+++ day-14/simple-python-app/start_container.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
 set -e
 
-CONTAINER_NAME="simple-python-flask-app"
+CONTAINER_NAME="danduniharika_simple_python"
+IMAGE_NAME="danduniharika30/simple-python-flask-app"
 
 # Stop and remove existing container if it exists
 if [ "$(docker ps -q -f name=$CONTAINER_NAME)" ]; then
-    echo "Stopping existing container..."
     docker stop $CONTAINER_NAME
 fi
 
 if [ "$(docker ps -aq -f name=$CONTAINER_NAME)" ]; then
-    echo "Removing existing container..."
     docker rm $CONTAINER_NAME
 fi
 
-# Pull the latest Docker image
-docker pull danduniharika30/simple-python-flask-app
+# Pull latest image
+docker pull $IMAGE_NAME
 
-# Run the Docker container
-docker run -d --name $CONTAINER_NAME -p 5000:5000 danduniharika30/simple-python-flask-app
+# Run container
+docker run -d --name $CONTAINER_NAME -p 5000:5000 $IMAGE_NAME
+
+echo "Container $CONTAINER_NAME started with image $IMAGE_NAME"
